0cb8fda3d977a2f93fc89423274f7aa5cabb11f0
bombr <47834716+bombr90@users.noreply.github.com>
2023-06-01 22:26:58 -0700

    Implemented category controller/views
    -improved styling
    -implemented category logic
    -coded various views

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -4,36 +4,162 @@ const Item = require("../models/item");
 const Category = require("../models/category");
 
 const category_detail = asyncHandler(async (req, res, next) => {
-  res.send("category_detail not implemented");
+  const [category, itemsInCategory] = await Promise.all([
+    Category.findById(req.params.id).exec(),
+    Item.find({ category: req.params.id }, "name description stock").exec(),
+  ]);
+  if(category === null) {
+    const err = new Error("Category not found");
+    err.status = 404;
+    return next(err);
+  }
+  res.render("category_detail", {
+    title: "Category Detail",
+    category,
+    itemsInCategory, 
+  })
 });
 
 const category_list = asyncHandler(async (req, res, next) => {
-  res.send("category_list not implemented");
+  const categoryList = await Category.find().sort({ name: 1 }).exec();
+  
+  res.render("category_list", {
+    title: "Category List",
+    categoryList,
+  });
 });
 
 const category_create_get = asyncHandler(async (req, res, next) => {
-  res.send("category_create_get not implemented");
+  res.render("category_form", { title: "Create Category" });
 });
 
-const category_create_post = asyncHandler(async (req, res, next) => {
-  res.send("category_create_post not implemented");
-});
+const category_create_post = [
+  // Validate and sanitize the name field.
+  body("name", "Category name must contain at least 3 characters")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+
+  // Process request after validation and sanitization.
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a genre object with escaped and trimmed data.
+    const category = new Category({ name: req.body.name });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.render("category_form", {
+        title: "Create Category",
+        category,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid.
+      // Check if Category with same name already exists.
+      const categoryExists = await Category.findOne({ name: req.body.name }).exec();
+      if (categoryExists) {
+        // Genre exists, redirect to its detail page.
+        res.redirect(categoryExists.url);
+      } else {
+        await category.save();
+        // New genre saved. Redirect to genre detail page.
+        res.redirect(category.url);
+      }
+    }
+  }),
+];
+
 
 const category_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("category_delete_get not implemented");
+ const [category, categoryItems] = await Promise.all([
+   Category.findById(req.params.id).exec(),
+   Item.find({ category: req.params.id }, "name description").exec(),
+ ]);
+ if (category === null) {
+   // No results.
+   res.redirect("/inventory/categories");
+ }
+
+ res.render("category_delete", {
+   title: "Delete Category",
+   category,
+   categoryItems,
+ });  
 });
 
 const category_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("category_delete_post not implemented");
+  // Get details of Category and all its items (in parallel)
+  const [category, categoryItems] = await Promise.all([
+    Category.findById(req.params.id).exec(),
+    Item.find({ category: req.params.id }, "name description").exec(),
+  ]);
+
+  if (categoryItems.length > 0) {
+    // Category has items. Render in same way as for GET route.
+    res.render("category_delete", {
+      title: "Delete Category",
+      category,
+      categoryItems
+    });
+    return;
+  } else {
+    // Category has no items. Delete object and redirect to the list of category.
+    await Category.findByIdAndRemove(req.body.categoryid);
+    res.redirect("/inventory/categories");
+  }
 });
 
 const category_update_get = asyncHandler(async (req, res, next) => {
-  res.send("category_update_get not implemented");
+  const category = await Category.findById(req.params.id).exec();
+  if(category === null){
+    const err = new Error("Category not found");
+    err.status=404;
+    return next(err);
+  }
+
+  res.render("category_form", { 
+    title: "Update Category",
+    category,
+  });
 });
 
-const category_update_post = asyncHandler(async (req, res, next) => {
-  res.send("category_update_post not implemented");
-});
+const category_update_post = [
+  // Validate and sanitize the name field.
+  body("name", "Category name must contain at least 3 characters")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
+
+  // Process request after validation and sanitization.
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a genre object with escaped and trimmed data.
+    const category = new Category({ 
+      name: req.body.name,
+      _id: req.params.id, 
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.render("category_form", {
+        title: "Create Category",
+        category,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid.
+      //Update category and redirect
+        await Category.findByIdAndUpdate(req.params.id, category);
+        res.redirect(category.url);
+    }
+  }),
+];
 
 module.exports = {
   category_detail,
--- controllers/itemController.js
+++ controllers/itemController.js
@@ -19,15 +19,33 @@ const index = asyncHandler(async(req,res,next) => {
 })
 
 const item_detail = asyncHandler(async(req,res,next)=>{
-  res.send("item_detail not implemented");
+  const item = await Item.findById(req.params.id).populate("category").exec();
+
+  if(item===null){
+    const err = new Error("Item not found");
+    err.status= 404;
+    return next(err);
+  }
+  res.render("item_detail", {
+    title: item.name,
+    item,
+  })
 })
 
 const item_list = asyncHandler(async (req, res, next) => {
-  res.send("item_list not implemented");
+  const itemList = await Item.find().sort({ name: 1 }).exec();
+  res.render("item_list", {
+    title: "Item List",
+    itemList,
+  });
 });
 
 const item_create_get = asyncHandler(async (req, res, next) => {
-  res.send("item_create_get not implemented");
+  const categoryList = await Category.find().sort({name:1}).exec();
+  res.render("item_form", {
+    title: 'Create Item',
+    categoryList,
+  })
 });
 
 const item_create_post = asyncHandler(async (req, res, next) => {
